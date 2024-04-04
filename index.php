54af9b821de65f48ce6c3c3559bc5c4c122e43e3
tioya2923 <30002076@students.ual.pt>
2024-04-05 00:00:34 +0100

    04042024.1

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-echo file_get_contents('frontend/public/index.html');
+// Defina o caminho para o arquivo index.html na pasta build
+$index_file_path = 'frontend/build/index.html';
+
+// Verifique se o arquivo existe
+if (file_exists($index_file_path)) {
+    // Leia o conteúdo do arquivo
+    $content = file_get_contents($index_file_path);
+
+    // Imprima o conteúdo do arquivo
+    echo $content;
+} else {
+    echo "O arquivo $index_file_path não existe";
+}
 ?>
