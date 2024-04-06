947cdb174c553c60aa9c6ad5249cb9bab8b3ec11
tioya2923 <30002076@students.ual.pt>
2024-04-06 22:08:00 +0100

    subir o php

--- backend/connect/server.php
+++ backend/connect/server.php
@@ -4,10 +4,18 @@
 include '../connect/cors.php';
 
 // Conectar ao banco de dados
-$servername = "localhost";
-$username = "root";
-$password = "19101989";
-$dbname = "happy_family";
+$servername = "us-cluster-east-01.k8s.cleardb.net";
+$username = "b022ba9ba77c40";
+$password = "90c1f4f6";
+$dbname = "heroku_2c4225e1b3df742";
+
+
+
+// CONEXÃO LOCAL:
+// $servername = "localhost";
+// $username = "root";
+// $password = "19101989";
+// $dbname = "happy_family";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
