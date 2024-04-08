ea1a2ca6d73a154392465c6627d5744722f4602f
tioya2923 <30002076@students.ual.pt>
2024-04-08 02:20:06 +0100

    08042024.13

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Defina o caminho para o arquivo index.html na pasta build
-$index_file_path = 'frontend/public/';
+$index_file_path = 'frontend/build/index.html';
 
 // Verifique se o arquivo existe
 if (!file_exists($index_file_path)) {
