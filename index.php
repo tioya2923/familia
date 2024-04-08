a2829ae8d3ed1442ddadaad0a5ef7ea4ee15e035
tioya2923 <30002076@students.ual.pt>
2024-04-08 01:03:57 +0100

    08042024.6

--- index.php
+++ index.php
@@ -3,20 +3,20 @@
 $index_file_path = 'frontend/build/index.html';
 
 // Verifique se o arquivo existe
-if (file_exists($index_file_path)) {
-    // Leia o conteúdo do arquivo e imprima
-    echo file_get_contents($index_file_path);
-} else {
+if (!file_exists($index_file_path)) {
     // Se o arquivo não existir, exiba uma mensagem de erro
-    echo "O arquivo $index_file_path não existe";
+    die("O arquivo $index_file_path não existe");
 }
 
 // Adicione um manipulador para todas as rotas não encontradas
 // Isso irá capturar qualquer rota não definida e servir o index.html do React
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-if ($uri !== '/' && file_exists($_SERVER['DOCUMENT_ROOT'].$uri)) {
-    return false;
-} else {
-    echo file_get_contents($index_file_path);
+
+// Verifique se a solicitação é para um arquivo estático ou para uma rota API
+if ($uri !== '/' && !preg_match('/^\/api/', $uri) && file_exists($_SERVER['DOCUMENT_ROOT'].$uri)) {
+    return false; // Serve o arquivo estático
 }
+
+// Para todas as outras rotas, sirva o index.html do React
+echo file_get_contents($index_file_path);
 ?>
