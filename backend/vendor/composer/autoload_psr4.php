e170e400f573492a1a4c3f8fbcbc1bc1c2b7b520
tioya2923 <30002076@students.ual.pt>
2024-04-08 17:57:10 +0100

    08042024.16

--- backend/vendor/composer/autoload_psr4.php
+++ backend/vendor/composer/autoload_psr4.php
@@ -7,4 +7,12 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Yavam\\Server\\' => array($baseDir . '/src'),
+    'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
+    'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-factory/src', $vendorDir . '/psr/http-message/src'),
+    'Psr\\Http\\Client\\' => array($vendorDir . '/psr/http-client/src'),
+    'JmesPath\\' => array($vendorDir . '/mtdowling/jmespath.php/src'),
+    'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
+    'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
+    'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
+    'Aws\\' => array($vendorDir . '/aws/aws-sdk-php/src'),
 );
--- backend/vendor/composer/autoload_real.php
+++ backend/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInita1859f98c5792576445beb9eb8806553
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInita1859f98c5792576445beb9eb8806553', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInita1859f98c5792576445beb9eb8806553', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInita1859f98c5792576445beb9eb8806553
         call_user_func(\Composer\Autoload\ComposerStaticInita1859f98c5792576445beb9eb8806553::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInita1859f98c5792576445beb9eb8806553::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
