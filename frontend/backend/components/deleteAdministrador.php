508b0cff0f87cb118e24157ed4f63598d9bb8a70
tioya2923 <30002076@students.ual.pt>
2024-04-09 00:26:14 +0100

    09042024.1

--- frontend/backend/components/deleteAdministrador.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../connect/server.php';
-require_once '../connect/cors.php';
-include './updateAdministradores.php';
-
-$id = $_GET['id_admin'];
-$current_user_id = $_GET['current_user_id']; // Obtenha o ID do usuário atual
-deleteAdmin($id, $current_user_id); // Passe ambos os IDs para a função
-?>
--- frontend/backend/components/deleteUsuario.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../connect/server.php';
-require_once '../connect/cors.php';
-include './updateUsuarios.php';
-
-
-$id = $_GET['id'];
-deleteUser($id);
-?>
