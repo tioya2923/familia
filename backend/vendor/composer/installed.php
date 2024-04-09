43f8fb3850e75f8149854682ac42848b9c08cf67
tioya2923 <30002076@students.ual.pt>
2024-04-09 02:57:51 +0100

    08042024.16

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '508b0cff0f87cb118e24157ed4f63598d9bb8a70',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '508b0cff0f87cb118e24157ed4f63598d9bb8a70',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
